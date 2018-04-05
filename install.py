88db0c91896e10167fc2bb48ac2792a5f43e918e
Bruno Roy <brusi_roy@hotmail.com>
2018-04-05 12:56:58 -0400

    Add basic copying script

--- install.py
+++ install.py
@@ -1,8 +1,18 @@
 import colorama
+import os
+from pathlib import Path
 import platform
+import shutil
+import sys
 import yaml
 
 colorama.init()
+
+def print_ok(msg):
+    print(f'{colorama.Fore.GREEN}[OK] {colorama.Style.RESET_ALL}{msg}')
+
+def print_warn(msg):
+    print(f'{colorama.Fore.YELLOW}[WARN] {colorama.Style.RESET_ALL}{msg}')
 
 def print_error(msg):
     print(f'{colorama.Fore.RED}[ERROR] {colorama.Style.RESET_ALL}{msg}')
@@ -29,6 +39,7 @@ class Config:
             current_platform = platform.system()
 
             if current_platform in config['platform']:
+                self.platform = current_platform
 
                 if 'src' in config['platform'][current_platform]:
                     self.src = config['platform'][current_platform]['src']
@@ -46,6 +57,40 @@ class Config:
         else:
             print_error(f'Missing tag `platform` in `{config_file_path}`')
 
+        self._convert_home_tilde()
+
+
+    def _convert_home_tilde(self):
+        """Converts the '~' in the path by the actual HOME path"""
+        self.src = [src.replace('~', str(Path.home())) for src in self.src]
+        self.dest = [dest.replace('~', str(Path.home())) for dest in self.dest]
+
+    def _file_exists(self, src, dest):
+        # TODO: More input validation
+        # TODO: Add more options, like move to a `.old` file
+        print_warn(f'File `{dest}` already exists... Do you want to remove it? (y/N)')
+        variable = str(input()).lower().strip()
+        if variable == 'y':
+            self._copy_file(src, dest)
+        elif variable == 'n' or variable == '':
+            # TODO:
+            pass
+        else:
+            print_error('Invalid input')
+
+    def _copy_file(self, src, dest):
+        shutil.copy2(src, dest)
+        print_ok(f'Successfully copied `{src}` to `{dest}`')
+
+    def install(self):
+        print(f'Installing {self.name} on the {self.platform} platform')
+        for src, dest in zip(self.src, self.dest):
+            if os.path.isfile(dest):
+                self._file_exists(src, dest)
+            else:
+                self._copy_file(src, dest)
+
 
 config = Config('config_example.yaml')
+config.install()
 
